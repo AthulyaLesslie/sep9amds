5c567663f8c5788284f6a3a3a41dd44d4b7c7888
AthulyaLesslie <athulyalesslie@gmail.com>
2025-02-14 10:42:30 +0530

    msg

--- data1.py
+++ data1.py
@@ -5,10 +5,10 @@ x=int(input('enter first integer:'))
 y=int(input('enter second integer:'))
 print('sum is',add(x,y))
 
-def sub(x,y):
-   return x-y
+def mul(x,y):
+   return x*y
 
 x=int(input('enter first integer:'))
 y=int(input('enter second integer:'))
-print('difference is',sub(x,y))
+print('product is',mul(x,y))
 
